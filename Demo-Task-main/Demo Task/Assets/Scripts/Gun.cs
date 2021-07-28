13cd1a0bccf181aa28f8b15086999b405a3bd995
Akashshukla95 <39201187+Akashshukla95@users.noreply.github.com>
2021-07-28 11:47:32 +0530

    Added Particle System

--- Demo-Task-main/Demo Task/Assets/Scripts/Gun.cs
+++ Demo-Task-main/Demo Task/Assets/Scripts/Gun.cs
@@ -5,19 +5,27 @@ public class Gun : MonoBehaviour
 	public float damage = 10f;
 	public float range = 100f;
 
+	public float fireRate = 15f;
 	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+	public GameObject impactEffect;
+	public float impactForce;
+
+	private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
 		{
+			nextTimeToFire = Time.time + 1f / fireRate;
 			Shoot();
 		}
     }
 
 	void Shoot()
 	{
+		muzzleFlash.Play(); 
 		RaycastHit hit;
 		if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
 		{
@@ -26,6 +34,14 @@ public class Gun : MonoBehaviour
 			{
 				target.TakeDamage(damage);
 			}
+
+			if(hit.rigidbody != null)
+			{
+				hit.rigidbody.AddForce(-hit.normal * impactForce);
+			}
+
+			GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+			Destroy(impactGO, .5f);
 		}
 	}
 }
